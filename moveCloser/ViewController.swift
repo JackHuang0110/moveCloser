978a66948af099340b369e2f40b542a938dc07da
Jack Huang <jackhuang@Jackde-MacBook-Air.local>
2018-08-16 16:56:14 +0800

    add music

--- moveCloser/ViewController.swift
+++ moveCloser/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
     @IBOutlet weak var pagyTextField: UITextField!
     @IBOutlet weak var marioTextField: UITextField!
     @IBOutlet weak var marioImageView: UIImageView!
     @IBOutlet weak var pagyImageView: UIImageView!
+    
+    var myPlayer:AVAudioPlayer?
+    
+    
+    
     @IBAction func moveCloserSlider(_ sender: UISlider) {
         marioImageView.frame.origin.x = CGFloat(58 + 160 * sender.value)
         pagyImageView.frame.origin.x = CGFloat(503 - 160 * sender.value)
         if sender.value == Float(0.94) {
             pagyTextField.isHidden = false
             marioTextField.isHidden = false
+            
+         
+            myPlayer?.stop()
+            myPlayer?.currentTime = 0.0
+            myPlayer?.play()
+            
         }else{
             pagyTextField.isHidden = true
             marioTextField.isHidden = true
@@ -27,7 +38,13 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let path = Bundle.main.path(forResource: "theyear", ofType: "mp3"){
+            do {
+                myPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            }catch{
+                myPlayer = nil
+            }
+        }        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
