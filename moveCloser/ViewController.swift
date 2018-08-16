90e171dd7baa0fb3348d64bb3ee6a5234c5d7e6e
Jack Huang <jackhuang@Jackde-MacBook-Air.local>
2018-08-16 15:54:00 +0800

    new movement app

--- moveCloser/ViewController.swift
+++ moveCloser/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pagyTextField: UITextField!
+    @IBOutlet weak var marioTextField: UITextField!
+    @IBOutlet weak var marioImageView: UIImageView!
+    @IBOutlet weak var pagyImageView: UIImageView!
+    @IBAction func moveCloserSlider(_ sender: UISlider) {
+        marioImageView.frame.origin.x = CGFloat(58 + 160 * sender.value)
+        pagyImageView.frame.origin.x = CGFloat(503 - 160 * sender.value)
+        if sender.value == Float(0.94) {
+            pagyTextField.isHidden = false
+            marioTextField.isHidden = false
+        }else{
+            pagyTextField.isHidden = true
+            marioTextField.isHidden = true
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
